21d5d2b3035b9744b6500bb917f372f0dbb88d10
fans <fs1762920@163.com>
2019-03-07 00:28:52 +0800

    注釋信息

--- webpack.config.js
+++ webpack.config.js
@@ -6,24 +6,24 @@ module.exports = {
     entry: './src/js/entry.js',   // 入口文件
     output: {                     // 输出配置
         filename: 'bundle.js',      // 输出文件名
-        path: path.resolve(__dirname, 'dist')   //输出文件路径配置
+        path: path.resolve(__dirname, 'dist')   //输出文件路径配置 resolve(arg1, arg2)兩個參數前面不能帶“/”
     },
     module: {
         rules: [
             {
                 test: /\.css$/,
                 use: [
-                    'style-loader',
-                    'css-loader'
+                    'style-loader', //把css樣式加載到頁面中
+                    'css-loader' //加載css文件
                 ]
             },
             {
                 test: /\.(png|jpg|gif)$/,
                 use: [
                     {
-                        loader: 'url-loader',
+                        loader: 'url-loader', //加載url對應的資源
                         options: {
-                        limit: 8192      
+                        limit: 8192    //文件大小小於8*1024byte時就自動轉換為base64編碼放在頁面對應位置  
                         }
                     }
                 ]
@@ -31,7 +31,7 @@ module.exports = {
         ]
     },
     devServer: {
-        contentBase: './dist'
+        contentBase: './dist' //内置服务器动态加载页面所在的目录
     },
     plugins: [
         new CleanWebpackPlugin(['dist']),
